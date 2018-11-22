040cd4cfd9b000b0bfff1487138f97e4930322f9
Kwansu <bicsuok@gmail.com>
2018-11-22 05:33:07 +0000

    delete

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class PostList(ListView):
     model = Post
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['image', 'content', ]
+    #template_name = 'defaul.html' 지정 안하면 post_form을 찾습니다.
+    
+class PostDetail(DetailView):
+    model = Post
+    
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['image', 'content', ]
+    
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:list')
